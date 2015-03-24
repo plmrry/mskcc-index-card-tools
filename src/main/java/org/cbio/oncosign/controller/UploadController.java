d6b319ca6ad3aac69a35acd2c0d25b1befb929d8
onursumer <s.onur.sumer@gmail.com>
2015-03-23 21:47:01 -0400

    started implementing the R service

--- src/main/java/org/cbio/oncosign/controller/UploadController.java
+++ src/main/java/org/cbio/oncosign/controller/UploadController.java
@@ -5,8 +5,6 @@ import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.cbio.oncosign.service.OncosignService;
-import org.rosuda.REngine.Rserve.RConnection;
-import org.rosuda.REngine.Rserve.RserveException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -39,9 +37,6 @@ public class UploadController
 		this.oncosignService = oncosignService;
 	}
 
-	private RConnection conn = null;
-	protected boolean reloadData = true;
-
 	@RequestMapping(value = "file/",
 	                method = {RequestMethod.POST},
 	                headers = "Accept=multipart/form-data")
@@ -59,7 +54,7 @@ public class UploadController
 
 			String result = "NA";
 
-			// TODO process each file: write contents to outputDir and then pass them to R!
+			// process each file and write contents to outputDir
 			while (iter.hasNext())
 			{
 				FileItem item = iter.next();
@@ -73,10 +68,16 @@ public class UploadController
 				InputStream contentStream = item.getInputStream();
 
 				try {
-					//result = this.getUploadContextService().getStudyData(method, size, contentStream);
+					this.getOncosignService().copyFileContent(contentStream, fieldName);
 				} catch (Exception e) {
 					return new ResponseEntity<String>(e.getMessage(), headers, HttpStatus.BAD_REQUEST);
 				}
+			}
+
+			try {
+				result = this.getOncosignService().executeSea();
+			} catch (Exception e) {
+				return new ResponseEntity<String>(e.getMessage(), headers, HttpStatus.BAD_REQUEST);
 			}
 
 			return new ResponseEntity<String>(result, headers, HttpStatus.OK);
@@ -89,44 +90,5 @@ public class UploadController
 		{
 			return new ResponseEntity<String>("File upload error.", headers, HttpStatus.BAD_REQUEST);
 		}
-	}
-
-	/**
-	 * Returns the existing r connection. If connection is not init yet,
-	 * initializes a new R connection with default libraries and data.
-	 *
-	 * @return  RConnection object
-	 * @throws IOException
-	 * @throws org.rosuda.REngine.Rserve.RserveException
-	 */
-	protected RConnection getRConn() throws IOException, RserveException
-	{
-		// init R connection if not init yet
-		if (this.conn == null)
-		{
-			// init connection
-			RConnection c = new RConnection();
-			this.conn = c;
-
-			// load required libraries
-			c.voidEval("library(qvalue);");
-			c.voidEval("library(oncosign);");
-
-			// TODO load required source files if required
-			//String functions = this.getrFunctionResource().getFile().getAbsolutePath();
-			// c.voidEval("source('" + functions + "');");
-
-			// TODO load data files
-			//String pcData = this.getPcDataResource().getFile().getAbsolutePath();
-			//c.voidEval("pcData <- readRDS('" + pcData + "');");
-		}
-
-		if (this.reloadData)
-		{
-			// TODO reload data files if required
-			//this.readDataMatrix(this.conn);
-		}
-
-		return this.conn;
 	}
 }
--- src/main/java/org/cbio/oncosign/service/OncosignService.java
+++ src/main/java/org/cbio/oncosign/service/OncosignService.java
@@ -1,11 +1,18 @@
 package org.cbio.oncosign.service;
 
+import flexjson.JSONSerializer;
+import org.apache.commons.fileupload.InvalidFileNameException;
+import org.cbio.oncosign.util.IOUtil;
+import org.rosuda.REngine.REXP;
 import org.rosuda.REngine.REXPMismatchException;
+import org.rosuda.REngine.RList;
 import org.rosuda.REngine.Rserve.RConnection;
 import org.rosuda.REngine.Rserve.RserveException;
 import org.springframework.core.io.Resource;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Service to retrieve result for user uploaded data as a graph.
@@ -14,7 +21,7 @@ import java.io.*;
  */
 public class OncosignService
 {
-	public static String OUTPUT_FILENAME = "captonviz_upload.txt";
+	public static String OUTPUT_EXTENSION = "txt";
 
 	// source directory for temporary file output
 	private Resource outputDirResource;
@@ -29,20 +36,103 @@ public class OncosignService
 		this.outputDirResource = outputDirResource;
 	}
 
+	// oncosign properties file
+	private Resource propsResource;
+
+	public Resource getPropsResource()
+	{
+		return propsResource;
+	}
+
+	public void setPropsResource(Resource propsResource)
+	{
+		this.propsResource = propsResource;
+	}
+
+	private RConnection conn = null;
+	protected boolean reloadData = true;
+
+	public void copyFileContent(InputStream is, String fieldName) throws IOException, InvalidFileNameException
+	{
+		if (!IOUtil.isValidFilename(fieldName))
+		{
+			throw new InvalidFileNameException(fieldName, "Invalid filename.");
+		}
+
+		String outputDir = this.getOutputDirResource().getFile().getAbsolutePath();
+
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(is));
+
+		BufferedWriter writer = new BufferedWriter(
+				new FileWriter(outputDir + "/" + fieldName + "." + OUTPUT_EXTENSION));
+
+		String line = null;
+
+		while ((line = reader.readLine()) != null)
+		{
+			writer.write(line);
+			writer.newLine();
+		}
+
+		reader.close();
+		writer.close();
+	}
 	/**
-	 * Reads data matrix user input.
+	 * Returns the existing r connection. If connection is not init yet,
+	 * initializes a new R connection with default libraries and data.
 	 *
-	 * @param c RConnection
-	 * @throws java.io.IOException
+	 * @return  RConnection object
+	 * @throws IOException
 	 * @throws org.rosuda.REngine.Rserve.RserveException
 	 */
-	protected void readDataMatrix(RConnection c) throws IOException, RserveException
+	protected RConnection getRConn() throws IOException, RserveException
 	{
-		String dataFile = this.getOutputDirResource().getFile().getAbsolutePath() +
-		                  "/" + OUTPUT_FILENAME;
+		// init R connection if not init yet
+		if (this.conn == null)
+		{
+			// init connection
+			RConnection c = new RConnection();
+			this.conn = c;
 
-		c.voidEval("uploadedData <- read.csv('" + dataFile + "', header=TRUE, sep='\t');");
-		c.voidEval("dataMatrix <- uploadedData[,-1];");
-		c.voidEval("rownames(dataMatrix) <- uploadedData[,1];");
+			// load required libraries
+			c.voidEval("library(qvalue);");
+			c.voidEval("library(oncosign);");
+
+			// TODO load required source files if required
+			//String functions = this.getrFunctionResource().getFile().getAbsolutePath();
+			// c.voidEval("source('" + functions + "');");
+
+			// TODO load data files
+			//String pcData = this.getPcDataResource().getFile().getAbsolutePath();
+			//c.voidEval("pcData <- readRDS('" + pcData + "');");
+		}
+
+		if (this.reloadData)
+		{
+			// TODO reload data files if required
+			//this.readDataMatrix(this.conn);
+		}
+
+		return this.conn;
+	}
+
+	public String executeSea() throws REXPMismatchException, RserveException, IOException
+	{
+		RConnection c = this.getRConn();
+		String props = this.getPropsResource().getFile().getAbsolutePath();
+
+		RList rList = c.eval("oncosign.sea('"+ props +"');").asList();
+
+		Map<String, REXP> map = new HashMap<>();
+
+		// TODO need to properly convert to JSON!
+		for (String key: rList.keys())
+		{
+			map.put(key, rList.at(key));
+		}
+
+		JSONSerializer jsonSerializer = new JSONSerializer().exclude("*.class");
+		return jsonSerializer.deepSerialize(map);
 	}
 }
