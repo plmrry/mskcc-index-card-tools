a700bf41402af6487c7e083864b047fc4caaf713
Ozgun Babur <ozgunbabur@gmail.com>
2015-05-20 17:19:06 -0400

    Initial commit for the card extractor. Does not compile yet.

---
+++ extractor/src/main/java/org/pathwaycommons/pathwaycards/extractor/SIFGenerator.java
@@ -0,0 +1,8 @@
+package org.pathwaycommons.pathwaycards.extractor;
+
+/**
+ * @author Ozgun Babur
+ */
+public class SIFGenerator
+{
+}
---
+++ web/src/main/java/org/pathwaycommons/pathwaycards/web/service/JSONReader.java
@@ -0,0 +1,8 @@
+package org.pathwaycommons.pathwaycards.web.service;
+
+/**
+ * @author Ozgun Babur
+ */
+public class JSONReader
+{
+}
