724efe4ed7beb8ce19a1423af205bfd0cd08b954
Onur Sumer <s.onur.sumer@gmail.com>
2015-06-03 19:29:39 -0400

    redefined the main comparison flow

--- comparator/src/main/scripts/IndexCardComparator.js
+++ comparator/src/main/scripts/IndexCardComparator.js
@@ -15,26 +15,54 @@ var IndexCardComparator = function()
 	 */
 	function compareCards(pcCards, inferenceCards)
 	{
+		var updatedCards = [];
+
 		// process pc cards to generate maps
 		_.each(pcCards, function(pcCard, idx) {
 			updateIdMap(_paIdMap, participantA(pcCard), pcCard);
 			updateIdMap(_pbIdMap, participantB(pcCard), pcCard);
 		});
 
+		// TODO currently we ignore participant A...
 		// for each inference card find matching PC card(s)
 		_.each(inferenceCards, function(inferenceCard, idx) {
-			// TODO find matching cards using the maps
+			// find matching cards for the participant b
+			var familyMembersFn = null; // TODO define a proper function
+			var queryIds = extractAllIds(participantB(inferenceCard), familyMembersFn);
+			var matchingCards = findMatchingCards(queryIds, inferenceCard, _pbIdMap);
+			var updatedCard = findModelRelation(inferenceCard, matchingCards);
+			updatedCards.push(updatedCard);
 		});
+
+		return updatedCards;
+	}
+
+	function findModelRelation(inferenceCard, matchingCards)
+	{
+		// TODO determine model relation wrt interaction type
+		return inferenceCard;
 	}
 
 	/**
 	 *
-	 * @param pcCard
+	 * @param queryIds
 	 * @param inferenceCard
+	 * @param idMap
 	 */
-	function isMatch(pcCard, inferenceCard)
+	function findMatchingCards(queryIds, inferenceCard, idMap)
 	{
+		var matchingCards = [];
 
+		_.each(queryIds, function(id, idx) {
+			_.each(idMap[id], function(card, idx) {
+				if (inferenceCard["interaction_type"] === card["interaction_type"])
+				{
+					matchingCards.push(card);
+				}
+			});
+		});
+
+		return matchingCards;
 	}
 
 	/**
@@ -46,20 +74,45 @@ var IndexCardComparator = function()
 	 */
 	function updateIdMap(idMap, participant, indexCard)
 	{
+		var ids = extractAllIds(participant);
+
+		_.each(ids, function(id, idx) {
+			if (id != null)
+			{
+				if (idMap[id] == null)
+				{
+					idMap[id] = [];
+				}
+
+				idMap[id].push(indexCard);
+			}
+		});
+	}
+
+	function extractAllIds(participant, familyMembersFn)
+	{
+		// set a default family member extraction function if none provided
+		familyMembersFn = familyMembersFn || function(participant) {
+			return participant["family_members"];
+		};
+
+		var ids = [];
+		var familyMembers = familyMembersFn(participant);
+
 		// if participant is an array then it is a complex
 		if (_.isArray(participant))
 		{
 			_.each(participant, function(ele, idx) {
-				updateIdMap(idMap, ele, indexCard);
+				ids = ids.concat(extractAllIds(ele, familyMembersFn));
 			});
 		}
 		// if entity type is protein family,
 		// then we need to use family_members array
 		else if (participant["entity_type"] == "protein_family" &&
-		         participant["family_members"] != null)
+		         familyMembers != null)
 		{
-			_.each(participant["family_members"], function(ele, idx) {
-				updateIdMap(idMap, ele, indexCard);
+			_.each(familyMembers, function(ele, idx) {
+				ids = ids.concat(extractAllIds(ele, familyMembersFn));
 			});
 		}
 		// else it is a simple participant
@@ -70,14 +123,11 @@ var IndexCardComparator = function()
 			// if the identifier field exists
 			if (id != null)
 			{
-				if (idMap[id] == null)
-				{
-					idMap[id] = [];
-				}
-
-				idMap[id].push(indexCard);
+				ids.push(id);
 			}
 		}
+
+		return ids;
 	}
 
 	function participantA(indexCard)
